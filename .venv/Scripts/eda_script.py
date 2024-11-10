8bf21a5f608225ed03f4fa060516af8aaae4c114
zulker0010 <zulkeryusuf@gmail.com>
2024-11-10 19:39:19 +0600

    wrote function for multiple pairplot

--- .venv/Scripts/eda_script.py
+++ .venv/Scripts/eda_script.py
@@ -1,5 +1,15 @@
 import seaborn as sns
 from clean_data import df
 
-car_to_bike = sns.pairplot(df, vars=["CarCount", "BikeCount"], hue="BikeCount")
+#pairplot for two variables 
+car_to_bike = sns.pairplot(df, vars=["CarCount", "BikeCount"],hue="Total")
 car_to_bike
+
+#pairplot for multiple variables
+sns.set_style("whitegrid")
+multiple_pairplot = sns.pairplot(df, vars=["CarCount","BikeCount", "BusCount", "TruckCount", "Total"],
+                                 hue="Traffic Situation", 
+                                 diag_kind="kde", 
+                                 palette="rocket",
+                                 height=3, aspect=1.2,
+                                 plot_kws={"alpha":0.6})
